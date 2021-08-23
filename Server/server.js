f03cae8d55f4c593971683ce7473b013e562e679
SorcerReyn <ryanshef01@gmail.com>
2021-08-23 19:08:10 -0400

    Create App.js
    Created app.js, and modified file names and positions for index.html and styles.css.

--- Server/server.js
+++ Server/server.js
@@ -1,23 +1,24 @@
-import { listenerCount } from 'process';
-
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(80);
+var path = require('path');
+
+app.use(express.static("public"));
 
 app.get('/', function(req, res){
-	res.sendFile('index.html');
+  res.sendFile('./app.component.html', {root: path.join(__dirname, '../Client/src/app')});
 });
 
 io.on('connection', function(socket){
-	console.log('user connected');
-	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
-	});
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
-	});
+  console.log('user connected');
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
-http:listenerCount(3000, function(){
-	console.log('listening on *:3000');
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
